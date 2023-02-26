b71c5d017c5f1b67178b09a1da866b6cf5697239
9Octopus <9octopusbrains@gmail.com>
2023-02-25 22:10:01 -0500

    Warmer

--- src/main/java/net/nineocto/tossmod/util/client/tossHandler.java
+++ src/main/java/net/nineocto/tossmod/util/client/tossHandler.java
@@ -2,13 +2,12 @@ package net.nineocto.tossmod.util.client;
 
 import com.mojang.math.Vector3d;
 import net.minecraft.client.Minecraft;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
 import net.nineocto.tossmod.util.network.packet.TossC2SPacket;
@@ -56,16 +55,33 @@ public class tossHandler {
         ItemStack thrownStack = stackInHand.copy();
         thrownStack.setCount(1);
 
+
         Vector3d playerPos = new Vector3d(player.getX(), player.getY(), player.getZ());
         Vec2 playerRotation = player.getRotationVector();
-        double throwDistance = 1.5; // tweak this value to adjust how far the item is thrown
-        double throwHeight = 0.5; // tweak this value to adjust how high the item is thrown
+        double throwDistance = 1.0; // adjust as needed
+        double throwAngle = 10.0; // adjust as needed
+        double throwHeight = 0.5; // adjust as needed
 
+        double xRot = Math.toRadians(playerRotation.x);
+        double yRot = Math.toRadians(playerRotation.y);
+
+        double x = -Math.sin(yRot) * Math.cos(xRot);
+        double y = -Math.sin(xRot);
+        double z = Math.cos(yRot) * Math.cos(xRot);
+
+        double horizontalDistance = Math.sqrt(x * x + z * z);
+        double horizontalVelocity = throwDistance / horizontalDistance;
+        double verticalVelocity = throwHeight / (throwDistance * Math.sin(Math.toRadians(throwAngle)) + throwHeight);
+
+        Vec3 motion = new Vec3(x * horizontalVelocity, y * verticalVelocity, z * horizontalVelocity);
         ItemEntity thrownItem = new ItemEntity(player.level, playerPos.x, playerPos.y + player.getEyeHeight(player.getPose()) - 0.2, playerPos.z, thrownStack);
-        double x = -Math.sin(Math.toRadians(player.getYRot())) * Math.cos(Math.toRadians(player.getXRot()));
-        double y = -Math.sin(Math.toRadians(player.getXRot()));
-        double z = Math.cos(Math.toRadians(player.getYRot())) * Math.cos(Math.toRadians(player.getXRot()));
-        thrownItem.setDeltaMovement(x * throwDistance, y * throwHeight, z * throwDistance);
+        thrownItem.setDeltaMovement(motion);
+
+        float yaw = (float) Math.toDegrees(Math.atan2(-x, z));
+        float pitch = (float) Math.toDegrees(Math.atan2(-y, horizontalDistance));
+        thrownItem.setYRot(yaw);
+        thrownItem.setXRot(pitch);
+        thrownItem.setPickUpDelay(20);
         player.level.addFreshEntity(thrownItem);
     }
 
@@ -75,7 +91,6 @@ public class tossHandler {
         }
 
         while (tossKeyMapping.consumeClick()) {
-            Minecraft.getInstance().player.playSound(SoundEvents.INK_SAC_USE, 1.0f, 1.0f);
             ItemStack stackInHand = Minecraft.getInstance().player.getItemInHand(InteractionHand.MAIN_HAND);
             tossmodPacketHandler.sendToServer(new TossC2SPacket(stackInHand));
             stackInHand.shrink(1);
