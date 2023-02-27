44a2f74c460e86e5b427505039e4bafde0a768ea
9Octopus <9octopusbrains@gmail.com>
2023-02-26 21:28:42 -0500

    1.0

--- src/main/java/net/nineocto/tossmod/util/client/KeyBindings.java
+++ src/main/java/net/nineocto/tossmod/util/client/KeyBindings.java
@@ -1,10 +1,12 @@
 package net.nineocto.tossmod.util.client;
 
+import com.mojang.blaze3d.platform.InputConstants.Type;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 
+import static net.minecraftforge.client.settings.KeyConflictContext.IN_GAME;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_TAB;
 
 //This simply registers our keybind, It is triggered in Client Events.
@@ -14,7 +16,7 @@ public class KeyBindings {
         @OnlyIn(Dist.CLIENT)
         public static void init(RegisterKeyMappingsEvent event) {
             tossKeyMapping = new KeyMapping(
-                    KEY_TOSS, GLFW_KEY_TAB, "key.categories.inventory"
+                    KEY_TOSS, IN_GAME, Type.KEYSYM, GLFW_KEY_TAB, "key.categories.inventory"
             );
             event.register(tossKeyMapping);
         }
